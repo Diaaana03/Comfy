1b623da50575f15b36ddd72c0e47a94613396d0a
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2023-10-08 22:18:46 +0100

    finish

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -7,12 +7,10 @@ const toggleCartBtn = getElement(".toggle-cart");
 toggleCartBtn.addEventListener("click", () => {
   cartOverlay.classList.add("show");
 });
-
 closeCartBtn.addEventListener("click", () => {
   cartOverlay.classList.remove("show");
 });
 
-export const openCart = (id) => {
+export const openCart = () => {
   cartOverlay.classList.add("show");
-  openCart();
 };
